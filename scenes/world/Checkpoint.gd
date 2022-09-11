4e4892e30f52810c896576ce99e670d7a3f7416e
HeartBox12 <maximosity1@gmail.com>
2022-09-11 13:36:20 -0700

    Created Checkpoint
    There is now a singleton that will track checkpoints as the player moves through them.

---
+++ scenes/world/Checkpoint.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready():
+	pass
+
+func _on_animation_finished():
+	$Display.animation = "Idle"
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		$Display.play()
+		Progression.checkpoint = self
